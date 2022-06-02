8e43e9e66464ea56d18a53590c7939c24e0d2fba
beaserter <beaserter@gmail.com>
2022-06-02 21:38:40 +0200

    nuevo archivo pruebas bst

--- Jenkinsfile.jsl.groovy
+++ Jenkinsfile.jsl.groovy
@@ -1,41 +1,26 @@
+library(
+      identifier: 'unir-jsl@master',
+      retriever: modernSCM(
+        [
+          $class: 'GitSCMSource',
+          remote: "https://github.com/srayuso/unir-jsl.git"
+        ]
+      )
+    ) _
+
 pipeline {
-    agent {
-        label 'docker'
-    }
-
+    agent any
     stages {
-            stage('Source') {
-                steps {
-                    git 'https://github.com/beaserter/unir-cicd.git'
-            }
-        }
-
-        stage('Build') {
+        stage('Commit info') {
             steps {
-                echo 'Building stage!'
-                sh 'make build'
+                jslInfo()
             }
         }
-
-        stage('Unit tests') {
+        stage('Repo details') {
             steps {
-                sh 'make test-unit'
-                archiveArtifacts artifacts: 'results/*.xml'
+                echo "Repository name: ${jslGit.getRemoteRepoName()}"
+                echo "Repository owner: ${jslGit.getRemoteRepoOwner()}"
             }
-        }
-
-        stage('Unit api') {
-            steps {
-                sh 'make test-api'
-                archiveArtifacts artifacts: 'results/*.xml'
-            }
-        }
-    }
-    
-    post {
-        always {
-            junit 'results/*_result.xml'
-            cleanWs()
         }
     }
 }
