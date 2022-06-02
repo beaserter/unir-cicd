d998918b3cac8ad95c8295b174a2f724c69e00cb
beaserter <beaserter@gmail.com>
2022-06-02 20:58:59 +0200

    Update Jenkinsfile.jsl.groovy

--- Jenkinsfile.jsl.groovy
+++ Jenkinsfile.jsl.groovy
@@ -1,27 +1,29 @@
 pipeline {
-    agent any
-        stages {
+    agent {
+        label = 'docker'
+    }
+    stages {
             stage('Source') {
                 steps {
                     git 'https://github.com/beaserter/unir-cicd.git'
+            }
         }
-    }
-    stage('Build') {
-        steps {
-            echo 'Building stage!'
-            sh 'make build'
+        stage('Build') {
+            steps {
+                echo 'Building stage!'
+                sh 'make build'
+            }
         }
-    }
-    stage('Unit tests') {
-        steps {
-            sh 'make test-unit'
-            archiveArtifacts artifacts: 'results/*.xml'
+        stage('Unit tests') {
+            steps {
+                sh 'make test-unit'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
         }
-    }
-    stage('Unit api') {
-        steps {
-            sh 'make test-api'
-            archiveArtifacts artifacts: 'results/*.xml'
+        stage('Unit api') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
             }
         }
     }
