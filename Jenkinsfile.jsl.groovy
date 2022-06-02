a32a216fb50b0233e3db18fd859464505c5bbf8f
beaserter <beaserter@gmail.com>
2022-06-02 20:50:18 +0200

    Update Jenkinsfile.jsl.groovy

--- Jenkinsfile.jsl.groovy
+++ Jenkinsfile.jsl.groovy
@@ -1,33 +1,34 @@
-library(
-      identifier: 'unir-jsl@master',
-      retriever: modernSCM(
-        [
-          $class: 'GitSCMSource',
-          remote: "https://github.com/srayuso/unir-jsl.git"
-        ]
-      )
-    ) _
-
 pipeline {
     agent any
-    stages {
-        stage('Commit info') {
-            steps {
-                jslInfo()
-            }
+        stages {
+            stage('Source') {
+                steps {
+                    git 'https://github.com/Anselm82/unir-cicd.git'
         }
-        stage('Repo details') {
-            steps {
-                echo "Repository name: ${jslGit.getRemoteRepoName()}"
-                echo "Repository owner: ${jslGit.getRemoteRepoOwner()}"
+    }
+    stage('Build') {
+        steps {
+            echo 'Building stage!'
+            sh 'make build'
+        }
+    }
+    stage('Unit tests') {
+        steps {
+            sh 'make test-unit'
+            archiveArtifacts artifacts: 'results/*.xml'
+        }
+    }
+    stage('Unit api') {
+        steps {
+            sh 'make test-api'
+            archiveArtifacts artifacts: 'results/*.xml'
             }
         }
     }
     post {
-	    always {
-	        echo "Clean..."
+        always {
+            junit 'results/*_result.xml'
             cleanWs()
         }
-    
     }
 }
