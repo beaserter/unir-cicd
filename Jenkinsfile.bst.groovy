cf1a71f29c7b8b70b210238a505b85fd87c5ff80
beaserter <beaserter@gmail.com>
2022-06-06 15:41:47 +0200

    Update Jenkinsfile.bst.groovy

--- Jenkinsfile.bst.groovy
+++ Jenkinsfile.bst.groovy
@@ -61,13 +61,11 @@ pipeline {
             cleanWs()
         }
         success {
-            emailext body: 'Test Message Success', subject: "Pipeline successful", to: "beatriz.serrano532@comunidadunir.net"
+            emailext body: 'Test Message Success', subject: "Pipeline successful", to: "devs@unir.net"
         }
-        /*
         unstable {
             emailext body: 'Test Message Unstable', subject: "Pipeline tests not successful", to: "devs@unir.net"
         }
-        */
         failure {
             emailext body: 'Test Message Fail', subject: "Pipeline error", to: "devops@unir.net,devs@unir.net"
         }
