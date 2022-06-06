f9f429d314c6b794ec683d357c300b879ae3cac0
beaserter <beaserter@gmail.com>
2022-06-06 11:30:32 +0200

    Update Jenkinsfile.simple.groovy

--- Jenkinsfile.simple.groovy
+++ Jenkinsfile.simple.groovy
@@ -1,6 +1,6 @@
 node('agent01') {
     stage('Source') {
-        git 'https://github.com/srayuso/unir-test.git'
+        git 'https://github.com/beaserter/unir-cicd.git'
     }
     stage('Build') {
         echo 'Building stage!'
