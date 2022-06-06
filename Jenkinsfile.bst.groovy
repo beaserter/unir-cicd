0d0e3802567d537246d9811fee465cb2fa73a8a9
beaserter <beaserter@gmail.com>
2022-06-06 13:10:32 +0200

    Update Jenkinsfile.bst.groovy

--- Jenkinsfile.bst.groovy
+++ Jenkinsfile.bst.groovy
@@ -21,23 +21,9 @@ pipeline {
             }
         }
 
-        stage('Browser Test cypress') {
-            steps {
-                sh 'make test-cypress'
-                archiveArtifacts artifacts: 'results/*.xml'
-            }
-        }
-
         stage('Unit api') {
             steps {
                 sh 'make test-api'
-                archiveArtifacts artifacts: 'results/*.xml'
-            }
-        }
-
-        stage('Integration tests') {
-            steps {
-                sh 'make test-integration'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
