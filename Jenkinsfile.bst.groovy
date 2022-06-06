0848ea1a27a39456b3b23db9ff9b555ec344249f
beaserter <beaserter@gmail.com>
2022-06-06 13:43:40 +0200

    Update Jenkinsfile.bst.groovy

--- Jenkinsfile.bst.groovy
+++ Jenkinsfile.bst.groovy
@@ -14,19 +14,34 @@ pipeline {
             }
         }
 
-        stage('Unit tests') {
+        stage('test-unit') {
             steps {
                 sh 'make test-unit'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
 
-        stage('Unit api') {
+        stage('test-api') {
             steps {
                 sh 'make test-api'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+
+        stage('test-behavior') {
+            steps {
+                sh 'make test-behavior'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+
+        stage('test-e2e') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+
 
     }
     
@@ -35,12 +50,14 @@ pipeline {
             junit 'results/*_result.xml'
             cleanWs()
         }
+        /*
         success {
             emailext body: 'Test Message Success', subject: "Pipeline successful", to: "devs@unir.net"
         }
         unstable {
             emailext body: 'Test Message Unstable', subject: "Pipeline tests not successful", to: "devs@unir.net"
         }
+        */
         failure {
             emailext body: 'Test Message Fail', subject: "Pipeline error", to: "devops@unir.net,devs@unir.net"
         }
